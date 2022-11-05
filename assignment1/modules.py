a15a99b94f8e98dc2b80b2736d78bd6a49556932
Gerard <gerardPlanella@github.com>
2022-11-05 16:37:15 +0100

    Performed Kaiming Initialisation and forward pass for linear module

--- assignment1/modules.py
+++ assignment1/modules.py
@@ -20,10 +20,12 @@ You should fill in code into indicated sections.
 import numpy as np
 
 
+
 class LinearModule(object):
     """
     Linear module. Applies a linear transformation to the input data.
     """
+
 
     def __init__(self, in_features, out_features, input_layer=False):
         """
@@ -45,10 +47,53 @@ class LinearModule(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
+        self.params = {}
+        self.grads = {}
 
+        self.params["weight"] = np.zeros((out_features, in_features))
+        self.params["weight"] = self.kaiming_initialisation(self.params["weight"], first_layer=input_layer)
+        self.grads["weight"] = np.zeros((out_features, in_features))
+
+        self.params["bias"] = np.zeros((1, out_features))
+        self.grads["bias"] = np.zeros((1, out_features))
         #######################
         # END OF YOUR CODE    #
         #######################
+
+    
+    def kaiming_initialisation(self, tensor, mode = "fan_in", first_layer = False, rectifier_slope = None):
+      assert tensor.ndim >= 2
+
+      if rectifier_slope is None:
+        if first_layer:
+          rectifier_slope = 1
+        else:
+          rectifier_slope = 0
+
+      if tensor.ndim == 2:
+        fan_in  = tensor.shape[1]
+        fan_out = tensor.shape[0]
+      else:
+        num_input_fmaps = tensor.shape[1]
+        num_output_fmaps = tensor.size[0]
+        receptive_field_size = 1
+        if tensor.ndim > 2:
+            receptive_field_size = tensor[0][0].numel()
+        fan_in = num_input_fmaps * receptive_field_size
+        fan_out = num_output_fmaps * receptive_field_size
+
+      
+      tensor = np.random.normal(0, 1, tensor.shape)
+      if mode == "fan_in":
+        tensor *= np.sqrt(2/((1 + rectifier_slope**2)*fan_in))
+      elif mode == "fan_out":
+        tensor *= np.sqrt(2/((1 + rectifier_slope**2)*fan_out))
+      else:
+        raise ValueError("Invalid Fan mode selected")
+
+      return tensor
+
+
 
     def forward(self, x):
         """
@@ -68,7 +113,8 @@ class LinearModule(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
-
+        self.x = x
+        out = x @ self.params["weight"].T + self.params["bias"]
         #######################
         # END OF YOUR CODE    #
         #######################
