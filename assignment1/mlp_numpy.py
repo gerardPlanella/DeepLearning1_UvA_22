solved exploding gradients bug

assignment1/mlp_numpy.py
@@ -55,6 +55,7 @@ class MLP(object):
         self.modules = []
         features = [n_inputs] + n_hidden
 
+
         for i in range(len(features) - 1):
           self.modules.append(LinearModule(features[i], features[i+1], i == 0))
           print(f"Linear Module ({features[i]}, {features[i+1]}, {i == 0}))")
@@ -87,8 +88,15 @@ class MLP(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
+
+        #print("----- Forward ----- ")
         out = x
-        for module in self.modules:
+        for idx, module in enumerate(self.modules):
+          """
+          print(f"Distribution before layer {idx}-{type(module).__name__} is N({np.mean(out):.2f}, {np.std(out):.2f})")
+          if(isinstance(module, LinearModule)):
+            print(f"Weight Distribution before layer {idx}-{type(module).__name__} is N({np.mean(module.params['weight']):.2f}, {np.std(module.params['weight'])})")
+          """
           out = module.forward(out)
 
         #######################
@@ -111,10 +119,18 @@ class MLP(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
+
+        #print("----- Backward -----")
         dx = dout
-        for i in range(len(self.modules) - 1, 0, -1):
-          dx = self.modules[i].backward(dx)
-        return dx
+        for idx, module in enumerate(reversed(self.modules)):
+          """
+          print(f"dx Distribution before layer {idx}-{type(module).__name__} is N({np.mean(dx):.2f}, {np.std(dx):.2f})")
+          if(isinstance(module, LinearModule)):
+            print(f"Weight Gradient Distribution before layer {idx}-{type(module).__name__} is N({np.mean(module.grads['weight']):.2f}, {np.std(module.grads['weight'])})")
+          """
+          dx = module.backward(dx)
+        
+        return
         #######################
         # END OF YOUR CODE    #
         #######################

assignment1/modules.py
@@ -18,7 +18,7 @@ This module implements various modules of the network.
 You should fill in code into indicated sections.
 """
 import numpy as np
-
+import sys
 
 
 class LinearModule(object):
@@ -61,36 +61,14 @@ class LinearModule(object):
         #######################
 
     
-    def kaiming_initialisation(self, tensor, mode = "fan_in", first_layer = False, rectifier_slope = None):
+    def kaiming_initialisation(self, tensor, first_layer = False):
       assert tensor.ndim >= 2
 
-      if rectifier_slope is None:
-        if first_layer:
-          rectifier_slope = 1
-        else:
-          rectifier_slope = 0
 
-      if tensor.ndim == 2:
-        fan_in  = tensor.shape[1]
-        fan_out = tensor.shape[0]
-      else:
-        num_input_fmaps = tensor.shape[1]
-        num_output_fmaps = tensor.size[0]
-        receptive_field_size = 1
-        if tensor.ndim > 2:
-            receptive_field_size = tensor[0][0].numel()
-        fan_in = num_input_fmaps * receptive_field_size
-        fan_out = num_output_fmaps * receptive_field_size
+      numerator = 1 if first_layer else 2
+      var = np.sqrt(numerator/tensor.shape[1])
 
-      
-      tensor = np.random.normal(0, 1, tensor.shape)
-      if mode == "fan_in":
-        tensor *= np.sqrt(2/((1 + rectifier_slope**2)*fan_in))
-      elif mode == "fan_out":
-        tensor *= np.sqrt(2/((1 + rectifier_slope**2)*fan_out))
-      else:
-        raise ValueError("Invalid Fan mode selected")
-
+      tensor = np.random.normal(0, var, tensor.shape) 
       return tensor
 
 
@@ -113,6 +91,7 @@ class LinearModule(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
+
         self.act = x
         out = x @ self.params["weight"].T + self.params["bias"]
         #######################
@@ -142,6 +121,7 @@ class LinearModule(object):
         self.grads["weight"] = dout.T @ self.act
         self.grads["bias"] = np.sum(dout, axis = 0)
         dx = dout @  self.params["weight"]
+        
 
         #######################
         # END OF YOUR CODE    #
@@ -191,7 +171,9 @@ class ELUModule(object):
         #######################
         alpha = 1.0
         self.act = x
-        out = np.where(x <= 0, (alpha * (np.exp(x) - 1)), x)
+        out = np.where(x < 0, (alpha * (np.exp(x) - 1)), x)
+
+
         #######################
         # END OF YOUR CODE    #
         #######################
@@ -214,7 +196,7 @@ class ELUModule(object):
         # PUT YOUR CODE HERE  #
         #######################
         alpha = 1.0
-        dx = dout * np.where(self.act <= 0, alpha * np.exp(self.act), np.ones(self.act.shape))
+        dx = dout * np.where(self.act < 0, alpha * np.exp(self.act), np.ones(self.act.shape))
         #######################
         # END OF YOUR CODE    #
         #######################
@@ -292,8 +274,7 @@ class SoftMaxModule(object):
         prod = prod @ ones
         sub = dout - prod
         dx = np.einsum("ij, ij -> ij", self.act, sub)
-      
-    
+        
         
         #######################
         # END OF YOUR CODE    #
@@ -339,7 +320,7 @@ class CrossEntropyModule(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
-        out = -np.sum(y.reshape(-1, 1) * np.log(x)) / x.shape[0]
+        out = -np.sum(np.log(x[np.arange(y.shape[0]), y])) / x.shape[0]
         #######################
         # END OF YOUR CODE    #
         #######################
@@ -362,7 +343,9 @@ class CrossEntropyModule(object):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
-        dx = -1 * (y.reshape(-1, 1) * (1/x)) / x.shape[0]
+        dx = np.zeros(x.shape)
+        dx[np.arange(y.shape[0]), y] = (-1. / (x.shape[0] * x[np.arange(y.shape[0]), y]))
+
         #######################
         # END OF YOUR CODE    #
         #######################

assignment1/train_mlp_numpy.py
@@ -31,6 +31,8 @@ from modules import CrossEntropyModule, LinearModule
 import cifar10_utils
 
 import torch
+from collections import defaultdict
+import sys
 
 import sys
 
@@ -52,8 +54,9 @@ def confusion_matrix(predictions, targets, num_classes = 10):
 
     conf_mat = np.zeros((num_classes, num_classes))
     for element in range(len(targets)):
+      #print(predictions[element, :])
       predicted_class = np.argmax(predictions[element, :])
-      conf_mat[predicted_class, targets[element]]+=1
+      conf_mat[targets[element], predicted_class]+=1
       #print(f"Prediction: {predicted_class}, Target: {targets[element]}")
     
     #######################
@@ -76,33 +79,36 @@ def confusion_matrix_to_metrics(confusion_matrix, beta=1., num_classes = 10):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
+    
     metrics = {}
     n_classes = confusion_matrix.shape[0]
 
     tp = np.diag(confusion_matrix)
-    fp = np.sum(confusion_matrix, axis=1)- tp
-    fn = np.sum(confusion_matrix, axis=0) - tp
+    fn = np.sum(confusion_matrix, axis=1)- tp
+    fp = np.sum(confusion_matrix, axis=0) - tp
     tn =  np.sum(confusion_matrix) - (fp + fn + tp)
 
+    """
     print(f"TP: {tp}")
     print(f"FP: {fp}")
     print(f"TN: {tn}")
     print(f"FN: {fn}")
     print(f"Confusion Matrix: {confusion_matrix}")
+    """
 
     metrics["precision"] = tp /(tp + fp)
     metrics["recall"] = tp / (tp + fn)
-    metrics["accuracy_cls"] = (tp + tn) / (tp + fp + tn + fn)
-    metrics["accuracy"] = np.mean((tp + tn) / (tp + fp + tn + fn))
+    metrics["accuracy"] = np.trace(confusion_matrix) / np.sum(confusion_matrix)
 
     metrics["f1_beta"] = (1 + beta**2)*(metrics["precision"] * metrics["recall"]) / \
       (((beta**2) * metrics["precision"]) + metrics["recall"])
 
+    """
     print("Accuracy: " + str(metrics["accuracy"]))
-    print("Class Accuracies: " + str(metrics["accuracy_cls"]))
     print("Precision: " + str(metrics["precision"]))
     print("Recall: " + str(metrics["recall"]))
     print("F1: " + str(metrics["f1_beta"]))
+    """
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -116,8 +122,8 @@ def evaluate_model(model, data_loader, batch_size = 128, num_classes=10):
     Args:
       model: An instance of 'MLP', the model to evaluate.
       data_loader: The data loader of the dataset to evaluate.
-    Returns:
-        metrics: A dictionary calculated using the conversion of the confusion matrix to metrics.
+    Returns:        metrics: A dictionary calculated using the conversion of the confusion matrix to metrics.
+
 
     TODO:
     Implement evaluation of the MLP model on a given dataset.
@@ -134,7 +140,7 @@ def evaluate_model(model, data_loader, batch_size = 128, num_classes=10):
     conf_matrix = np.zeros((num_classes, num_classes))
     n_batches = 0
 
-    for batch_num, (x, t) in enumerate(data_loader):
+    for x, t in data_loader:
       x_flat = x.reshape(x.shape[0], -1)
       t_flat = t.reshape(-1, 1)
       prediction = model.forward(x_flat)
@@ -156,21 +162,23 @@ def train_model_SGD(model, loss_module, data_loader, lr):
   losses = []
   for batch_num, (x, t) in enumerate(data_loader):
     x_flat = x.reshape(x.shape[0], -1)
-    t_flat = t.reshape(-1, 1)
 
     y = model.forward(x_flat)
-    loss = loss_module.forward(y, t_flat)
+    loss = loss_module.forward(y, t)
     losses.append(loss)
 
-    dx = loss_module.backward(y, t_flat)
+    dx = loss_module.backward(y, t)
     model.backward(dx)
 
     for module in model.modules:
       if isinstance(module, LinearModule):
-        module.params["weight"] -= lr*module.grads["weight"]
-        module.params["bias"] -= lr*module.grads["bias"]
+        module.params["weight"] -= (lr*module.grads["weight"])
+        module.params["bias"] -= (lr*module.grads["bias"])
+      
+      
 
   return model, losses
+
 
 
 
@@ -231,14 +239,44 @@ def train(hidden_dims, lr, batch_size, epochs, seed, data_dir, input_size = 32*3
   best_accuracy = -1
   best_epoch = -1
   best_model = None
+  total_losses = defaultdict(list)
   for epoch in range(epochs):
     model, losses = train_model_SGD(model, loss_module, cifar10_loader["train"], lr)
+
+    """"
+    for idx, module in enumerate(model.modules) :
+      if isinstance(module, LinearModule):
+        if any(item is False for item in np.isnan(module.params["weight"])):
+          print(f"Weights NaN")
+        if any(item is False for item in np.isnan(module.params["bias"])):
+          print("Bias NaN")
+        if any(item is False for item in np.isnan(module.grads["weight"])):
+          print(f"Weights Grad NaN")
+        if any(item is False for item in np.isnan(module.grads["bias"])):
+          print("Bias Grad NaN")
+
+        print(f"----- Layer {idx} -----")
+        #if not np.all(module.params["weight"]):
+        print(f"Weights Param Zero: " +  str(module.params["weight"].size - np.count_nonzero(module.params["weight"] != 0)) + ", NonZero: " + str(np.count_nonzero(module.params["weight"] != 0)))
+        #if not np.all(module.params["bias"]):
+        print("Bias Param Zero: " + str(module.params["bias"].size - np.count_nonzero(module.params["bias"] != 0)) + ", NonZero: " + str(np.count_nonzero(module.params["bias"] != 0)))
+        #if not np.all(module.grads["weight"]):
+        print(f"Weights Grad Zero: " + str(module.grads["weight"].size - np.count_nonzero(module.grads["weight"] != 0)) + ", NonZero: " + str(np.count_nonzero(module.grads["weight"] != 0)))
+        #if not np.all(module.grads["bias"]):
+        print("Bias Grad Zero: " + str(module.grads["bias"].size - np.count_nonzero(module.grads["bias"] != 0)) + ", NonZero: " + str(np.count_nonzero(module.grads["bias"] != 0)))
+        
+        if(np.count_nonzero(module.params["weight"] != 0) == 0):
+          print(module.params["weight"])
+    """
+
+    total_losses[epoch] = losses
     metric = evaluate_model(model, cifar10_loader["validation"], n_classes)
     val_metrics.append(metric)
     val_accuracies.append(metric["accuracy"])
     if metric["accuracy"] > best_accuracy:
       best_epoch = epoch
       best_accuracy = metric["accuracy"]
+      model.clear_cache()
       best_model = deepcopy(model)
     print(f"Validation Accuracy for epoch {epoch} -> {metric['accuracy']}")
   # TODO: Test best model
@@ -256,7 +294,7 @@ def train(hidden_dims, lr, batch_size, epochs, seed, data_dir, input_size = 32*3
     }
   logging_info = {
     "info": info,
-    "train": {"losses": losses},
+    "train": {"losses": total_losses},
     "validation": val_metrics, 
     "test": test_metrics
     }
@@ -293,6 +331,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     kwargs = vars(args)
 
-    train(**kwargs)
+    best_model, val_accuracies, test_accuracy, logging_info = train(**kwargs)
+    
+
     # Feel free to add any additional functions, such as plotting of the loss curve here
     